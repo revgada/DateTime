062aca2b917eb43b7373201b7d59e00252ccc363
Ammar Rahic <ammarrahic@live.com>
2017-12-24 09:18:16 +0100

    Add getters and setters, and toString test

--- src/DateTime.java
+++ src/DateTime.java
@@ -6,7 +6,14 @@ public class DateTime {
     private int month;
     private int year;
 
-    public DateTime(int month, int day, int year) throws Exception {
+
+    /**
+     * DateTime(int, int, int) corresponds to standard mm/dd/yy date
+     * format.
+     * @throws Exception in case some or all of the parameters fall out
+     * of the required range.
+     */
+    public DateTime(int year, int month, int day) throws Exception {
         if (validDate(month, day, year)) {
             this.year = year;
             this.month = month;
@@ -17,7 +24,12 @@ public class DateTime {
 
     }
 
-
+    /**
+     * DateTime(int, int, int) corresponds to standard mm/dd/yy/hh/mm/ss
+     * date and time format.
+     * @throws Exception in case some or all of the parameters fall out
+     * of the required range.
+     */
     public DateTime(int year, int month, int day, int hour, int minute, int second) throws Exception {
         if (validDateTime(month, day, year, hour, minute, second)) {
             this.year = year;
@@ -30,4 +42,54 @@ public class DateTime {
             throw new Exception("Invalid DateTime");
         }
     }
+
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
 }
--- src/DateTimeTest.java
+++ src/DateTimeTest.java
@@ -1,6 +1,27 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DateTimeTest {
 
+    @Test
+    void DateTimeTest() {
+        try {
+            DateTime date = new DateTime(2012, 12, 21);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    void toStringDateTest(){
+        try {
+            DateTime date = new DateTime(2012, 12, 21);
+            assertEquals("12/21/2012", date.toString());
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 
 }
