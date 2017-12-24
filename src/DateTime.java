9407e5b749de386130748308720404792462624c
Ammar Rahic <ammarrahic@live.com>
2017-12-24 13:09:49 +0100

    Add constructors for Date and DateTime

--- src/DateTime.java
+++ src/DateTime.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
-
 public class DateTime {
+    private int second;
+    private int minute;
+    private int hour;
+    private int day;
+    private int month;
+    private int year;
+
+    public DateTime(int month, int day, int year) throws Exception {
+        if (validDate(month, day, year)) {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+        } else {
+            throw new Exception("Invalid Date");
+        }
+
+    }
+
+
+    public DateTime(int year, int month, int day, int hour, int minute, int second) throws Exception {
+        if (validDateTime(month, day, year, hour, minute, second)) {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        } else {
+            throw new Exception("Invalid DateTime");
+        }
+    }
 }
